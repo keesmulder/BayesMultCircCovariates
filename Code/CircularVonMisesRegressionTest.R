69bc7e4a4ae16da91864b95476de8b80eeb096a7
Kees Mulder <k.t.mulder@uu.nl>
2014-10-24 15:04:45 +0200

    Added a file that explores the weird shapes of the log-likelihood of beta.
    Reordered regression test, added and changed a lot

--- Code/CircularVonMisesRegressionTest.R
+++ Code/CircularVonMisesRegressionTest.R
@@ -1,4 +1,4 @@
-# rm(list=ls())
+rm(list=ls())
 
 library(Rcpp)
 library(circular)
@@ -10,64 +10,6 @@ sourceCpp("Code/rvmc.cpp")
 source("Code/describeCirc.R")
 source("Code/vonMises.R")
 
-printcount <- FALSE
-verbose <- FALSE
-
-# Link function
-linkfun    <- function(x) 4 * atan(x)
-invlinkfun <- function(x) tan(x/4)
-
-plot(linkfun, xlim=c(-10, 10))
-
-
-# Cov-matrix
-estCovMat <- function(X, bt, kp, linkfun, linkxbeta = apply(X, 1, "%*%", bt)) {
-  g      <- linkfun(linkxbeta)
-  G      <- diag(g)
-  Gsq    <- G %*% G
-  nc     <- 1 / (kp * A1(kp))
-  pt1    <- solve(t(X) %*% Gsq %*% X)
-  pt2num <- pt1 %*% t(X) %*% g %*% t(g) %*% X %*% pt1
-  pt2den <- n - (t(g) %*% X  %*% pt1  %*% t(X) %*% g)
-
-  nc * (pt1 + pt2num / as.numeric(pt2den))
-}
-
-
-# Data generation
-n       <- 100
-X       <- matrix(rnorm(n))
-true_mu <- 1
-true_bt <- -0.05
-err     <- rnorm(n, sd = 0.1)
-th      <- true_mu + linkfun(true_bt * X) + err
-
-
-plot(circular(th))
-hist(X, breaks=30)
-plot(X, atan(th))
-
-# Data summary statistics
-R <- sqrt(sum(cos(th))^2 + sum(sin(th))^2)
-mu_hat <- meanDir(th)
-
-# Settings for sampler
-mu_start <- mu_hat
-kp_start <- 1
-bt_start <- 2
-Q <- 1000
-p <- length(bt_start)
-
-
-# Empty matrices and initialization.
-mu <- c(mu_start, rep(NA, Q-1))
-kp <- c(kp_start, rep(NA, Q-1))
-bt <- rbind(bt_start, matrix(nc=p, nr=Q-1))
-row.names(bt) <- NULL
-mu_cur <- mu[1]
-kp_cur <- kp[1]
-bt_cur <- bt[1, ]
-
 # Joint Log-likelihood function, with data already built in.
 buildLogLikfun <- function(th, X, linkfun){
   function(mu, kp, bt){
@@ -77,153 +19,243 @@ buildLogLikfun <- function(th, X, linkfun){
   }
 }
 
+# bwb: Bandwith for the proposal for beta
+mcmcGCM <- function(th, X, linkfun, invlinkfun,
+                    mu_start=0, kp_start=1, bt_start=2,
+                    bwb=.05, Q=1000,
+                    printcount=FALSE, verbose=FALSE) {
+
+  # Empty matrices and initialization.
+  p <- length(bt_start)
+  mu <- c(mu_start, rep(NA, Q-1))
+  kp <- c(kp_start, rep(NA, Q-1))
+  bt <- rbind(bt_start, matrix(nc=p, nr=Q-1))
+  rownames(bt) <- NULL
+  colnames(bt) <- paste0("beta_", 1:p)
+  mu_cur <- mu[1]
+  kp_cur <- kp[1]
+  bt_cur <- bt[1, ]
+
+  # Log-likelihood for this dataset and link function.
+  llfun <- buildLogLikfun(th, X, linkfun)
+
+  for(it in 2:Q) {
+
+    if (printcount) print(paste("Start of iteration", it))
+
+    # linkxbeta is what the set of covariates adds in terms of predicted angle.
+    linkxbeta <- linkfun(apply(X, 1, "%*%", bt_cur))
+
+    # New properties
+    S_fromBeta  <- sum(sin(th - linkxbeta))
+    C_fromBeta  <- sum(cos(th - linkxbeta))
+    R_fromBeta  <- sqrt(C_fromBeta^2 + S_fromBeta^2)
+    mu_fromBeta <- atan2(S_fromBeta, C_fromBeta)
+
+    # Generate a new value for mu.
+    ### TODO: Mathematically prove this to be equal to the ll:
+    # Plots showing that this is true:
+    #   plot(xlim=c(-0.5, 7),
+    #        function(x) dvm(x, mu=mu_fromBeta, kappa = R_fromBeta*kp_cur))
+    #   plot(xlim=c(-0.5, 7), type="l",
+    #        circSeq,
+    #        sapply(circSeq, function(x) exp(llfun(x, kp=kp_cur, bt=bt_cur))))
+
+    if (is.na(mu_fromBeta) | kp_cur<1e-20) stop(paste("NA mu at it", it))
+    mu_cur <- rvmc(1, mu_fromBeta, R_fromBeta*kp_cur)
+
+    if (verbose) {
+      cat("\014")
+      print(paste("Start of iteration", it))
+      printVars(bt_cur, kp_cur, mu_cur)
+    }
 
 
-# Log-likelihood for this dataset and link function.
-llfun <- buildLogLikfun(th, X, linkfun)
+    #### BETA
+    #
+    #   estCov_cur <- estCovMat(X = X, bt = bt_cur, kp = kp_cur,
+    #                           linkfun = invlinkfun, linkxbeta = linkxbeta)
+    #   bt_can     <- mvtnorm::rmvnorm(n = 1, mean = bt_cur, sigma = estCov_cur)
+    #
+    #   bt_lratio <- llfun(mu = mu_cur, kp = kp_cur, bt = bt_can) +
+    #     dmvnorm(bt_cur, log = TRUE, mean = bt_can, sigma = estCov_cur) -
+    #     llfun(mu = mu_cur, kp = kp_cur, bt = bt_cur) -
+    #     dmvnorm(bt_can, log = TRUE, mean = bt_cur, sigma = estCov_cur)
+    bt_can     <- bt_cur + runif(1, -bwb, bwb)
 
-# Bandwith for the proposal for beta
-bwb <- .05
+    bt_lratio <- llfun(mu = mu_cur, kp = kp_cur, bt = bt_can)  -
+      llfun(mu = mu_cur, kp = kp_cur, bt = bt_cur)
 
-it <- 2
-for(it in 2:Q) {
+    if (bt_lratio > log(runif(1))) {
+      bt_cur <- bt_can
+    }
 
-  if (printcount) print(paste("Start of iteration", it))
+    if (verbose) {
+      print("After choosing new beta")
+      printVars(bt_cur, bt_can, estCov_cur)
+    }
 
-  # linkxbeta is what the set of covariates adds in terms of predicted angle.
-  linkxbeta <- linkfun(apply(X, 1, "%*%", bt_cur))
+    ### KAPPA
+    kp_can     <- rchisq(1, df = kp_cur)
 
-  # New properties
-  S_fromBeta  <- sum(sin(th - linkxbeta))
-  C_fromBeta  <- sum(cos(th - linkxbeta))
-  R_fromBeta  <- sqrt(C_fromBeta^2 + S_fromBeta^2)
-  mu_fromBeta <- atan2(S_fromBeta, C_fromBeta)
+    kp_lratio <- llfun(mu = mu_cur, kp = kp_can, bt = bt_cur) +
+      dchisq(kp_cur, df = kp_can, log = TRUE) -
+      llfun(mu = mu_cur, kp = kp_cur, bt = bt_cur) -
+      dchisq(kp_can, df = kp_cur, log = TRUE)
 
-  # Generate a new value for mu.
-  ### TODO: Mathematically prove this to be equal to the ll:
-  # Plots showing that this is true:
-  #   plot(xlim=c(-0.5, 7),
-  #        function(x) dvm(x, mu=mu_fromBeta, kappa = R_fromBeta*kp_cur))
-  #   plot(xlim=c(-0.5, 7), type="l",
-  #        circSeq,
-  #        sapply(circSeq, function(x) exp(llfun(x, kp=kp_cur, bt=bt_cur))))
 
-  if (is.na(mu_fromBeta) | kp_cur<1e-20) stop(paste("NA mu at it", it))
-  mu_cur <- rvmc(1, mu_fromBeta, R_fromBeta*kp_cur)
+    if (kp_lratio > log(runif(1))) {
+      kp_cur <- kp_can
+    }
 
-  if (verbose) {
-    cat("\014")
-    print(paste("Start of iteration", it))
-    printVars(bt_cur, kp_cur, mu_cur)
+    if (verbose) {
+      print("After choosing Kappa")
+      printVars(kp_cur, kp_can)
+    }
+
+
+
+
+    #### PRINTING ####
+    if(FALSE){
+
+
+      # Likelihood
+      bandwith <- .2
+      lim <- bt_cur + c(-1, 1) * bandwith
+
+      # # # # Beta
+      plot(Vectorize(function(x) exp(llfun(mu_cur, kp=kp_cur, bt=x))), xlim=lim)
+      # Proposal
+      plot(Vectorize(function(x) dunif(x, bt_cur-bwb, bt_cur+bwb)), xlim=lim)
+
+      #   #
+      lim <- c(-0.05, 200)
+
+      # # Kappa
+      #     Likelihood
+      plot(Vectorize(function(x) exp(llfun(mu_cur, kp=x, bt=bt_cur))), xlim=lim)
+      plot(Vectorize(function(x) exp(llfun(0.1, kp=x, bt=bt_cur))), xlim=lim)
+      plot(Vectorize(function(x) exp(llfun(0.1, kp=x, bt=0))), xlim=lim)
+      plot(Vectorize(function(x) exp(llfun(0.4, kp=x, bt=bt_cur))), xlim=lim)
+      plot(Vectorize(function(x) llfun(mu_cur, kp=x, bt=bt_cur)), xlim=lim)
+      # Proposal
+      plot(Vectorize(function(x) dchisq(x, df=kp_cur)), xlim=lim)
+      plot(Vectorize(function(x) dchisq(x, df=kp_can)), xlim=lim)
+    }
+
+    mu[it]   <- mu_cur
+    kp[it]   <- kp_cur
+    bt[it, ] <- bt_cur
+
+
   }
 
-
-  #### BETA
-  #
-  #   estCov_cur <- estCovMat(X = X, bt = bt_cur, kp = kp_cur,
-  #                           linkfun = invlinkfun, linkxbeta = linkxbeta)
-  #   bt_can     <- mvtnorm::rmvnorm(n = 1, mean = bt_cur, sigma = estCov_cur)
-  #
-  #   bt_lratio <- llfun(mu = mu_cur, kp = kp_cur, bt = bt_can) +
-  #     dmvnorm(bt_cur, log = TRUE, mean = bt_can, sigma = estCov_cur) -
-  #     llfun(mu = mu_cur, kp = kp_cur, bt = bt_cur) -
-  #     dmvnorm(bt_can, log = TRUE, mean = bt_cur, sigma = estCov_cur)
-  bt_can     <- bt_cur + runif(1, -bwb, bwb)
-
-  bt_lratio <- llfun(mu = mu_cur, kp = kp_cur, bt = bt_can)  -
-    llfun(mu = mu_cur, kp = kp_cur, bt = bt_cur)
-
-  if (bt_lratio > log(runif(1))) {
-    bt_cur <- bt_can
-  }
-
-  if (verbose) {
-    print("After choosing new beta")
-    printVars(bt_cur, bt_can, estCov_cur)
-  }
-
-  ### KAPPA
-  kp_can     <- rchisq(1, df = kp_cur)
-
-  kp_lratio <- llfun(mu = mu_cur, kp = kp_can, bt = bt_cur) +
-    dchisq(kp_cur, df = kp_can, log = TRUE) -
-    llfun(mu = mu_cur, kp = kp_cur, bt = bt_cur) -
-    dchisq(kp_can, df = kp_cur, log = TRUE)
-
-
-  if (kp_lratio > log(runif(1))) {
-    kp_cur <- kp_can
-  }
-
-  if (verbose) {
-    print("After choosing Kappa")
-    printVars(kp_cur, kp_can)
-  }
-
-
-
-
-  #### PRINTING ####
-  if(FALSE){
-
-
-    # Likelihood
-    bandwith <- .2
-    lim <- bt_cur + c(-1, 1) * bandwith
-
-    # # # # Beta
-    plot(Vectorize(function(x) exp(llfun(mu_cur, kp=kp_cur, bt=x))), xlim=lim)
-    # Proposal
-    plot(Vectorize(function(x) dunif(x, bt_cur-bwb, bt_cur+bwb)), xlim=lim)
-
-    #   #
-    lim <- c(-0.05, 200)
-
-    # # Kappa
-    #     Likelihood
-    plot(Vectorize(function(x) exp(llfun(mu_cur, kp=x, bt=bt_cur))), xlim=lim)
-    plot(Vectorize(function(x) llfun(mu_cur, kp=x, bt=bt_cur)), xlim=lim)
-    # Proposal
-    plot(Vectorize(function(x) dchisq(x, df=kp_cur)), xlim=lim)
-    plot(Vectorize(function(x) dchisq(x, df=kp_can)), xlim=lim)
-  }
-
-  mu[it]   <- mu_cur
-  kp[it]   <- kp_cur
-  bt[it, ] <- bt_cur
-
-  #   lines(x = c(it-1, it), y=c(bt[it-1], bt[it]))
-
-  #   rho <- min(1, llfun(mu_can, kp_can, bt_can) / llfun(mu[it-1], kp[it-1], bt[it-1, ]))
-
-  # it <- it+1
-
+  sapply(ls(), function(obj) assign(obj, get(obj), envir = .GlobalEnv))
+  return(data.frame(mu, kp, bt))
 }
 
 
 
-mu_cur
-kp_cur
-bt_cur
+# Link functions
+linkfun    <- function(x) 2 * atan(x)
+invlinkfun <- function(x) tan(x/2)
 
-plot.ts(mu)
-plot.ts(mu[-(1:100)])
-plot.ts(kp)
-plot.ts(bt)
+# Data generation
+n       <- 400
+
+# Re-find Gill & Hangartner's simulation
+true_mu <- 0
+true_bt <- 2
+true_kp <- 2
+
+X       <- matrix(runif(n, -1, 1))
+err     <- rvmc(n, mu = 0, kp = true_kp)
+
+th      <- (true_mu + linkfun(true_bt * X) + err) %% (2*pi)
+
+# Data summary statistics
+R      <- sqrt(sum(cos(th))^2 + sum(sin(th))^2)
+R_bar  <- R/n
+mu_hat <- meanDir(th)
+
+plot.new()
+par(mfrow=c(3, 3))
+
+
+# Show generated data
+plot(circular(th))
+# plot(linkfun, xlim=c(-10, 10))
+hist(X, breaks=30)
+plot(X, th)
+
+# Run sampler
+ s <- mcmcGCM(th = th, X = X, linkfun = linkfun, invlinkfun = invlinkfun)
+
+# Here, we can see that the estimate of kappa coincides with the residual variance
 # plot(circular(th))
+# plot(circular(th - linkfun(true_bt * X)))
 
-# mu_cur <- mean(circular(th))
+# Approx kappa of the full sample
+approxKappaML(th)
+A1inv(R_bar)
+
+# Approx residual kappa
+approxKappaML(circular(th - linkfun(true_bt * X)))
+
+# Plot sample results
+plot.ts(s$mu)
+plot.ts(s$kp)
+plot.ts(s$beta_1)
 
 
-mlebeta <- function(b) sum( (X * sin(th - mu_cur - linkfun(b * X))) / (1+linkfun(b * X)^2))
+# MLE BETA EXPLORATION
+mlebeta <- function(b) sum( (X * sin(th - mu_cur - linkfun(b * X))) / (1 + linkfun(b * X)^2))
 
-xl <- c(-3, 4)
-plot(Vectorize(mlebeta), xlim=xl) ; abline(h=0)
-plot(Vectorize(function(x) llfun(mu_cur, kp=kp_cur, bt=x)), xlim=xl)
+xl <- c(-150, 150)
+xl <- c(-15, 15)
 
-uniroot.all(Vectorize(mlebeta), interval = xl)
+mu_cur <- true_mu
+kp_cur <- true_kp
+# mu_cur <- pi/2
+# kp_cur <- 300
 
-optimize(f = function(x) llfun(mu_cur, kp=kp_cur, bt=x), interval = xl, maximum = TRUE)
+
+Rllfun <- function(bt) sum(cos(th - mu_cur - linkfun(apply(X, 1, "%*%", bt))))
+
+mleroot <- uniroot.all(Vectorize(mlebeta), interval = xl)
+
+
+llmax <- optimize(f = function(x) Rllfun(x), interval = xl, maximum = TRUE)$maximum
+llmin <- optimize(f = function(x) Rllfun(x), interval = xl, maximum = FALSE)$minimum
+
+mleroot
+c(llmin, llmax)
+
+
+plot(Vectorize(mlebeta), xlim=xl, main="MLEBETA function")
+abline(h=0, col="green")
+abline(v=mleroot, col="blue")
+abline(v=c(llmin, llmax), col="red")
+plot(Vectorize(function(x) Rllfun(x)), xlim=xl, main="LogLik of Beta")
+abline(v=mleroot, col="blue")
+abline(v=c(llmin, llmax), col="red")
+
+
+par(mfrow=c(1, 1))
+
+
+mus <- c(true_mu, true_mu, true_mu)
+bts <- c(llmin, llmax, -1500)
+
+plot(X, th)
+i <- 3
+for (i in seq(mus)) {
+  estth <- function(x) (mus[i] + linkfun(bts[i]*x)) %% (2*pi)
+  curve(estth, xlim=c(-1, 1), ylim = c(-pi, pi), add=TRUE, col=i)
+}
+
+
 
 
 
---
+++ Code/ShowBetaLikelihoodNotConcave.R
@@ -0,0 +1,66 @@
+# Re-find Gill & Hangartner's simulation
+rm(list=ls())
+
+library(Rcpp)
+library(circular)
+library(mvtnorm)
+library(ggplot2)
+library(rootSolve)
+
+sourceCpp("Code/rvmc.cpp")
+source("Code/describeCirc.R")
+source("Code/vonMises.R")
+
+# Link functions
+linkfun    <- function(x) 2 * atan(x)
+invlinkfun <- function(x) tan(x/2)
+
+# Data generation
+n       <- 1000
+true_mu <- pi
+true_bt <- .4
+true_kp <- 20
+X       <- matrix(rnorm(n, sd=0.2))
+err     <- rvmc(n, mu = 0, kp = true_kp)
+th      <- (true_mu + linkfun(true_bt * X) + err) %% (2*pi)
+
+plot.new()
+par(mfrow=c(2, 2))
+
+# Show generated data
+plotCircular(th)
+plot(X, th)
+
+# Limits
+xl <- c(-15000, 15000)
+xl <- c(-12, 12)
+
+test_mu <- pi/2
+test_mu <- pi
+test_mu <- true_mu
+
+
+# Main part of the loglikelihood function.
+Rllfun <- function(bt) sum(cos(th - test_mu - linkfun(apply(X, 1, "%*%", bt))))
+
+llmax <- optimize(f = function(x) Rllfun(x), interval = xl, maximum = TRUE)$maximum
+llmin <- optimize(f = function(x) Rllfun(x), interval = xl, maximum = FALSE)$minimum
+
+mus <- c(test_mu, test_mu, test_mu, test_mu)
+bts <- c(llmin, llmax, -5, 5)
+
+# Plot likelihood of beta at true mean and true kp, and add the extrema.
+plot(Vectorize(function(x) Rllfun(x)), xlim=xl, main="LogLik of Beta")
+abline(v=bts, col=2:(length(bts)+1))
+
+# Plot predictions for different beta's
+plot(X, th, xlim=c(min(X), max(X)), ylim = c(0, 2*pi),
+     main=paste("Given mu=", round(test_mu)))
+
+for (i in seq(mus)) {
+  estth <- function(x) (mus[i] + linkfun(bts[i]*x)) %% (2*pi)
+  curve(estth, add=TRUE, col=i+1)
+}
+abline(h=test_mu, col="blue", lty = 2)
+
+par(mfrow=c(1, 1))
